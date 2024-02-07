72c28bdfdd6872a2334c3ec741d0c149f86a5941
Stephane Ilic <stephane.ilic@gmail.com>
2024-02-07 12:55:19 +0100

    New function for tau calculation

--- LB_forecast_results/reiolib.py
+++ LB_forecast_results/reiolib.py
@@ -2,6 +2,17 @@ import numpy as np
 from tqdm import tqdm
 from scipy.interpolate import RegularGridInterpolator, PchipInterpolator
 
+# Constants
+_c_ =  2.99792458e8
+_Mpc_over_m_ = 3.085677581282e22
+_G_ = 6.67428e-11
+_m_H_ = 1.673575e-27
+_sigma_ = 6.6524616e-29
+_not4_ = 3.9715
+_k_B_ = 1.3806504e-23
+_h_P_ = 6.62606896e-34
+sigma_B = 2.*(np.pi**5.)*(_k_B_**4.)/15./(_h_P_**3.)/(_c_**2)
+T_cmb = 2.7255
 
 # YHe interpolator, accurate at 1e-7 level for massless neutrinos,
 # and 1e-4 for massive neutrinos (even for sum_mnu ~ 1 eV)
@@ -14,7 +25,6 @@ YHe_fct = RegularGridInterpolator((DeltaNeff_BBN, omegab_BBN),
                                   YHe_BBN, method="cubic")
 
 # Create fHe function from YHe interpolator
-_not4_ = 3.9715
 def fHe_fct(omega_b):
     YHe = YHe_fct((DeltaNeff_ref, omega_b))
     return YHe/(_not4_ * (1. - YHe))
@@ -82,3 +92,17 @@ def xe_flexalt(z, omega_b, z_f1, z_f2, xe_f2, z_f3):
     arg = (reio_he_z - z[:, None]) / reio_he_dz
     xe += fHe * (np.tanh(arg)+1.) / 2.
     return xe
+
+# Compute tau(z) integrand
+def tau_integ(z_arr, xe_arr, omega_b, H0, omega_cdm):
+    h = H0/100
+    YHe = YHe_fct((DeltaNeff_ref, omega_b))
+    n_e = 3 * (H0 * 1e3 / _Mpc_over_m_)**2. * (omega_b / h**2.) / (8*np.pi*_G_*_m_H_) * (1-YHe)
+    Omega_m = (omega_b + omega_cdm) / (H0/100)**2.
+    Omega_g = (4.*sigma_B/_c_*(T_cmb**4.))/(3.*_c_*_c_*1e10*h*h/_Mpc_over_m_/_Mpc_over_m_/8./np.pi/_G_)
+    Omega_ur = 3.044*7./8.*((4./11.)**(4./3.))*Omega_g
+    Omega_r = Omega_g + Omega_ur
+    Omega_l = 1. - Omega_m - Omega_g - Omega_r
+    H_arr = H0 * 1e3 * np.sqrt(Omega_m * (1+z_arr)**3. + Omega_r*(1+z_arr)**4. + Omega_l) / _Mpc_over_m_
+    comov = _c_ / H_arr
+    return n_e * xe_arr * comov * (1+z_arr)**2. * _sigma_
